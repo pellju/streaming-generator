c2dd68da6898fe87097d7df73636bfa877397f6a
pellju <juho.64@hotmail.com>
2024-05-04 18:30:10 +0300

    Installing CORS, added registration-routes and added MongoDB-examples to .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,19 @@
 import express, { Express, Request , Response} from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
-const app = express();
+const app: Express = express();
 const port = process.env.PORT;
+
+app.post('/register', (req: Request, res: Response) => {
+
+});
+
+app.get('/register', (req: Request, res: Response) => {
+    res.send({'Information': 'This is a TEST'})      
+});
 
 app.get('/', (req: Request, res: Response) => {
     res.send('This is a sample page');
